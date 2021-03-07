3c277cf5976a2618e567759139d52756a66ea607
OrelHamamy <Orelhamamy@gmail.com>
2021-03-07 18:37:04 +0200

    Create img recorder

--- scripts/check.py
+++ scripts/check.py
@@ -1,31 +1,58 @@
 #!/usr/bin/env python
 
 import rospy
+import os
 import cv2
 import numpy as np
 from std_msgs.msg import String, Int16
-from sensor_msgs.msg import CompressedImage
-from geometry_msgs.msg import PoseStamped
-import tf2_ros
-import tf2_geometry_msgs
-#from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image, CompressedImage
+from datetime import datetime
+from cv_bridge import CvBridge, CvBridgeError
 
-def main():
-    pose = PoseStamped()
-    pose.pose.position.x = 0.23
-    pose.pose.position.z = 0.92
-    pose.pose.orientation.w = 1
 
-    tf_buffer = tf2_ros.Buffer()
-    tf_listener = tf2_ros.TransformListener(tf_buffer)
-    transform = tf_buffer.lookup_transform('base_footprint',
-                                'kinect2_depth_optical_frame', #source frame
-                                rospy.Time(0), #get the tf at first available time
-                                rospy.Duration(1.0)) #wait for 1 second
-    pose_transformed = tf2_geometry_msgs.do_transform_pose(pose, transform)
-    print(pose_transformed)
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
 
+class image_convertor():
+    def __init__(self,camera_topic,frame_rate):
+        self.bridge = CvBridge()
+        self.shape = (128,128)
+        self.count = 0
+        self.date = datetime.now().strftime("%d-%m-%y,%H:%M")
+        rospy.Subscriber(camera_topic, Image, self.cb)
+
+
+    def cb(self, msg):
+        self.cv_img = self.bridge.imgmsg_to_cv2(msg,desired_encoding = 'passthrough')
+        self.resize_img  = cv2.resize(self.cv_img, self.shape,
+                               interpolation=cv2.INTER_LINEAR)
+        self.resize_img = self.resize_img/10*255
+        cv2.imshow('img', self.resize_img)
+        cv2.waitKey(2)
+
+    def save_img(self, event):
+        self.count +=1
+        cv2.imwrite('img/{}--{}.jpg'.format(self.date, self.count), self.resize_img)
+
+def main(frame_rate,camera_topic):
+    
+    image = image_convertor(camera_topic,frame_rate)
+    rospy.wait_for_message(camera_topic,Image)
+    rospy.Timer(rospy.Duration(1.0/float(frame_rate)), image.save_img)
+    rospy.spin()
+    cv2.destroyAllWindows()
 
 if __name__=='__main__':
-    rospy.init_node('alva', anonymous=True)
-    main()
+    rospy.init_node('video', anonymous=True)
+    frame_rate = rospy.get_param('frame_rate', default=15)
+    camera_topic = rospy.get_param('camera_topic', default= '/kinect2/qhd/image_depth_rect')
+    main(frame_rate,camera_topic)
+
+'''    #rospy.wait_for_message(camera_topic, Image)
+    while not rospy.is_shutdown():
+        #print((image.resize_img))
+        # out.write(np.uint8(image.resize_img))
+        #print(image.shape)
+        print()
+        '''
